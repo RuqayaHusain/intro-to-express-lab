21436661c8768635156e42633d4d6d3ef70bf52c
Ruqaya Husain <ruqayanader22@gmail.com>
2025-08-30 14:03:08 +0300

    done with all exercises

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const collectibles = [
     { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
 ];
 
-  const shoes = [
+const shoes = [
       { name: "Birkenstocks", price: 50, type: "sandal" },
       { name: "Air Jordans", price: 500, type: "sneaker" },
       { name: "Air Mahomeses", price: 501, type: "sneaker" },
@@ -18,12 +18,12 @@ const collectibles = [
       { name: "Velcro Sandals", price: 15, type: "sandal" },
       { name: "Jet Boots", price: 1000, type: "boot" },
       { name: "Fifty-Inch Heels", price: 175, type: "heel" }
-  ];
+];
 
 
-const getRandomNumber = (maxNumber) => {
-    //retruns random number between 0 & defined Max Number
-    return Math.floor(Math.random() * maxNumber);
+const getRandomNumber = (max) => {
+    //retruns random number between 0 & defined Max Number (inclusive)
+    return Math.floor(Math.random() * (max + 1));
 }
 
 app.listen(PORT, () => {
@@ -34,10 +34,11 @@ app.get('/greetings/:userName', (req, res) => {
     res.send(`<h1>Hello there, ${req.params.userName}!</h1>`);
 });
 
-app.get('/roll/:number', (req, res) => {
-    const inputValue = req.params.number;
+app.get('/roll/:inputNumber', (req, res) => {
+    let inputValue = req.params.inputNumber;
 
     if(validator.isNumeric(inputValue)){
+        inputValue = parseInt(inputValue);
         res.send(`<h1>You rolled a ${getRandomNumber(inputValue)}</h1>`);
     } else {
         res.send(`<h1>You must specify a number.</h1>`);
@@ -55,9 +56,22 @@ app.get('/collectibles/:index', (req, res) => {
 });
 
 app.get('/shoes', (req, res) => {
-    const minPrice = req.query.min-price;
-    const maxPrice = req.query.max-price;
+    const minPrice = req.query['min-price'];
+    const maxPrice = req.query['max-price'];
     const type = req.query.type;
+    let filteredShoes = shoes;
     
+    if(minPrice && validator.isInt(minPrice, { min: 0 })) {
+        filteredShoes = filteredShoes.filter(val => val.price >= parseInt(minPrice));
+    }
+
+    if(maxPrice && validator.isInt(maxPrice, { min: 0 })) {
+        filteredShoes = filteredShoes.filter(val => val.price <= parseInt(maxPrice));
+    }
     
+    if(type){
+        filteredShoes = filteredShoes.filter(val => val.type === type);
+    }
+
+    res.send(filteredShoes);
 });
